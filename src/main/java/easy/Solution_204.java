5282a32c636a7c535119daf55c7c3a742a73b631
JNU_MrC <1076878143@qq.com>
2020-04-25 13:08:53 +0800

    easy 至 problem 204

---
+++ src/main/java/easy/Solution_204.java
@@ -0,0 +1,40 @@
+package easy;
+
+/**
+ * 204. Count Primes
+ * Easy
+ * Count the number of prime numbers less than a non-negative number, n.
+ *
+ * @author mrc
+ * Date: 2020/4/25 0025
+ * Time: 12:46
+ */
+public class Solution_204
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Solution_204().countPrimes(5));
+        System.out.println(new Solution_204().countPrimes(10));
+        System.out.println(new Solution_204().countPrimes(30));
+    }
+    public int countPrimes(int n) {
+        if(n < 2) return 0;
+        boolean[] notPrimes = new boolean[n];
+        for (int i = 2; i < Math.sqrt(n); i++)
+        {
+            if(!notPrimes[i])
+            {
+                for (int j = 2; i * j < n; j++)
+                {
+                    notPrimes[i * j] = true;
+                }
+            }
+        }
+        int c = 0;
+        for (int i = 2; i < notPrimes.length; i++)
+        {
+            if(!notPrimes[i]) c++;
+        }
+        return c;
+    }
+}
